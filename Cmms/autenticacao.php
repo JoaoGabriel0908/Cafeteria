f3a9f849dd49fce0900d3ce5fc551c787e737028
João Gabriel <joaogabrielc700@gmail.com>
2022-03-25 23:42:26 -0300

    Cantina

---
+++ Cmms/autenticacao.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="CSS/reset.css">
+    <link rel="stylesheet" href="CSS/style.css">
+    <title>Document</title>
+</head>
+<body>
+    <section class="tela-autenticacao">
+        <div class="autenticacao">
+            <div class="texto-cms">
+                <p class="titulo">Autenticação para o CMS</p>
+                <p class="nomeProjeto">Cafeteria</p>
+            </div>
+            <form action="">
+                <div class="login">
+                    <p>Login:</p> 
+                    <input type="text" name="txtlogin" class="txt" value="">
+                </div>
+                <div class="senha">
+                    <p>Senha:</p>
+                    <input type="text" name="txtsenha" class="txt" value="">
+                </div>
+				<input type="submit" name="btnlogin" value="Login" class="button">
+            </form>
+        </div>
+    </section>
+</body>
+</html>
---
+++ Cmms/dashboard.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="CSS/reset.css">
+    <link rel="stylesheet" href="CSS/autenticacao.css">
+    <title>Dashboard</title>
+</head>
+<body>
+    <header>
+        <div class="container-inicio">
+            <div class="container-texto">
+                <h1>C M S</h1>
+                <p class="nomeProjeto">Cafeteria</p>
+                <p class="gerenciamento">Gerenciamento de Conteúdo do Site</p>
+            </div>
+            <div class="imagem">
+                <img src="IMG/coffee-cup.png" alt="" width="100px" height="100px">
+            </div>
+        </div>   
+    </header>
+    <section class="menu-color">
+        <div class="menu">
+            <div class="administradores">
+                <div>
+                    <img src="IMG/coffee (1).png" alt="">
+                    <p>Adm. de Produtos</p>
+                </div>
+                <div>
+                    <img src="IMG/coffee.png" alt="">
+                    <p>Adm. de Categorias</p>
+                </div>
+                <div>
+                    <img src="IMG/livros-de-contato.png" alt="">
+                    <p>Contatos</p>
+                </div>
+                <div>
+                    <img src="IMG/perfil.png" alt="">
+                    <p>Usuários</p>
+                </div>
+            </div>
+            <div class="logout">
+                <p class="bem-vindo">Bem-Vindo Nome do Usuário</p>
+                <img src="IMG/sair.png" alt="">
+                <p>Logout</p>
+            </div>
+        </div>   
+    </section>
+    <section>
+        <p>Titulo da Sessão</p>
+    </section>
+    <footer>
+        <div class="copyright">
+            &copy; Copyright 2021
+            <p>Todos os direitos reservados - Política de Privacidade</p>
+        </div>
+        <div class="desenvolvido">
+            <p>Desenvolvido por : João Gabriel</p> 
+            <p>Versão 1.0.0</p> 
+        </div>
+    </footer>
+</body>
+</html>
